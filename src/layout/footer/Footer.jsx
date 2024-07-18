5ecdf4c467ef5f0c3ee31cc31d16932096af003b
atilimkocak <atilimkocak@gmail.com>
2024-07-18 23:57:13 +0300

    Self Commit

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,8 +1,14 @@
+import Bottom from "./components/Bottom";
+
 export default function Footer() {
   return (
-    <footer className="bg-footer p-4 text-center text-white">
-      <div>Teknolojik Yemekler</div>
-      <div>2024 Teknolojik Yemekler</div>
-    </footer>
+    <div className="divide-y divide-slate-800">
+      <footer className="mt-16 flex w-full flex-col justify-end bg-footer p-4 pt-14 text-center text-white max-md:mt-10 max-md:max-w-full">
+        <div className="mx-auto w-[800px]">
+          <div>Teknolojik Yemekler</div>
+        </div>
+      </footer>
+      <Bottom />
+    </div>
   );
 }
---
+++ src/layout/footer/components/Bottom.jsx
@@ -0,0 +1,15 @@
+export default function Bottom() {
+  return (
+    <>
+      <div className="bg-footer py-3 text-white">
+        <div className="mx-auto flex w-[800px] justify-between">
+          <p>© 2024 Teknolojik Yemekler.</p>
+          <img
+            src="Assets/mile2-aseets/footer/twitter.svg"
+            className="brightness-[103%] contrast-[103%] hue-rotate-0 invert saturate-[42%] sepia-0 filter"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
