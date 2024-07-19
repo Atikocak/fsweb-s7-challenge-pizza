64bee6a6410243d9e77357a3f7d6cc1902c378c1
Atikocak <atilimkocak@gmail.com>
2024-07-19 17:24:47 +0300

    Navbar update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { navbar } from "./data/navData.json";
 export default function Navbar() {
   return (
     <nav className="mx-auto leading-8 text-dark-gray">
-      <ul className="my-3 grid grid-flow-row grid-cols-2 justify-center justify-items-center gap-4 gap-x-0 px-20 py-7 font-quattrocento text-sm sm:my-3 sm:flex sm:w-full sm:gap-2 sm:p-2">
+      <ul className="my-3 grid grid-flow-row grid-cols-2 justify-center justify-items-center gap-4 gap-x-0 px-20 py-7 font-quattrocento text-base sm:my-3 sm:flex sm:w-full sm:gap-1 sm:p-2">
         {navbar.map((item) => (
           <li
             key={item.id}
-            className="w-32 gap-2 rounded-full p-2 font-barlow text-dark-gray transition duration-300 hover:bg-dark-gray hover:text-white"
+            className="w-[135px] rounded-full p-2 font-barlow text-dark-gray transition duration-300 hover:bg-dark-gray hover:text-white"
           >
             <Link to="/order">
-              <div className="flex items-center gap-4 sm:justify-center">
+              <div className="flex items-center gap-2 sm:justify-center">
                 <img
                   loading="lazy"
                   src={item.imageUrl}
